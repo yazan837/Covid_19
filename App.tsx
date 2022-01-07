f4ecb7ccf2edc47e06da423c4bbae65175253692
yazan hasan <yazan@aratech.ae>
2022-01-07 15:25:30 +0200

    add splash screen

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import store from './redux/store';
 import {Provider} from 'react-redux';
 import MyStack from './src/navigation/StackNavigator';
+import SplashScreen from 'react-native-splash-screen';
+
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <Provider store={store}>
       <MyStack />
